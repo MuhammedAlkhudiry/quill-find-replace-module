2e3ef4d05e76ce9e6e231401f4a621ef6838ea3b
Muhammed Hamad Alkhudiry <41856121+MuhammedAlkhudiry@users.noreply.github.com>
2019-08-03 20:15:26 +0300

    Update SearchBlot.js

--- SearchBlot.js
+++ SearchBlot.js
@@ -1,12 +1,6 @@
 const Inline = Quill.import('blots/inline');
 
  class SearchedStringBlot extends Inline {
-
-    static create(value) {
-        let node = super.create(value);
-        node.contentEditable = 'false';
-        return node;
-    }
 }
 
 SearchedStringBlot.blotName = 'SearchedString';
