856357b106d57a3a1a8576d2cf1614139317cbfc
Muhammed Hamad Alkhudiry <41856121+MuhammedAlkhudiry@users.noreply.github.com>
2019-08-14 15:45:34 +0300

    Update Searcher.js

--- Searcher.js
+++ Searcher.js
@@ -1,98 +1,121 @@
 class Searcher {
+  constructor(quill) {
+    this.quill = quill;
+    this.container = document.getElementById("search-container");
 
-    static occurrencesIndices = [];
-    static currentIndex = 0;
-    static SearchedStringLength = 0;
-    static SearchedString = '';
+    document
+      .getElementById("search")
+      .addEventListener("click", this.search.bind(this));
+    document
+      .getElementById("replace")
+      .addEventListener("click", this.replace.bind(this));
+    document
+      .getElementById("replace-all")
+      .addEventListener("click", this.replaceAll.bind(this));
+  }
 
-    constructor(quill) {
-        this.quill = quill;
-        this.container = document.getElementById('search-container');
+  search() {
+    //  remove any previous search
+    Searcher.removeStyle();
+    Searcher.SearchedString = document.getElementById("search-input").value;
+    if (Searcher.SearchedString) {
+      let totalText = quill.getText();
+      let re = new RegExp(Searcher.SearchedString, "gi");
+      let match = re.test(totalText);
+      if (match) {
+        let indices = (Searcher.occurrencesIndices = totalText.getIndicesOf(
+          Searcher.SearchedString
+        ));
+        let length = (Searcher.SearchedStringLength =
+          Searcher.SearchedString.length);
 
-        document
-            .getElementById('search')
-            .addEventListener('click', this.search.bind(this));
-        document
-            .getElementById('replace')
-            .addEventListener('click', this.replace.bind(this));
-        document
-            .getElementById('replace-all')
-            .addEventListener('click', this.replaceAll.bind(this));
+        indices.forEach(index =>
+          quill.formatText(index, length, "SearchedString", true)
+        );
+      } else {
+        Searcher.occurrencesIndices = null;
+        Searcher.currentIndex = 0;
+      }
+    } else {
+      Searcher.removeStyle();
     }
+  }
 
-    search() {
-        //  remove any previous search
-        Searcher.removeStyle();
-        Searcher.SearchedString = document.getElementById('search-input').value;
-        if (Searcher.SearchedString) {
-            let totalText = quill.getText();
-            let re = new RegExp(Searcher.SearchedString, 'gi');
-            let match = re.test(totalText);
-            if (match) {
-                let indices = Searcher.occurrencesIndices = totalText.getIndicesOf(Searcher.SearchedString);
-                let length = (Searcher.SearchedStringLength = Searcher.SearchedString.length);
+  replace() {
+    if (!Searcher.SearchedString) return;
 
-                indices.forEach(index => quill.formatText(index, length, 'SearchedString', true)
-                );
-            }
-            else {
-                Searcher.occurrencesIndices = null;
-                Searcher.currentIndex = 0;
-            }
-        }
-        else {
-            Searcher.removeStyle();
-        }
-    }
+    // if no occurrences, then search first.
+    if (!Searcher.occurrencesIndices) this.search();
+    if (!Searcher.occurrencesIndices) return;
 
-    replace() {
-        if (!Searcher.SearchedString) return;
+    let indices = Searcher.occurrencesIndices;
 
-        // if no occurrences, then search first.
-        if (!Searcher.occurrencesIndices) this.search();
-        if (!Searcher.occurrencesIndices) return;
+    let oldString = document.getElementById("search-input").value;
+    let newString = document.getElementById("replace-input").value;
 
-        let indices = Searcher.occurrencesIndices;
+    quill.deleteText(indices[Searcher.currentIndex], oldString.length);
+    quill.insertText(indices[Searcher.currentIndex], newString);
+    quill.formatText(
+      indices[Searcher.currentIndex],
+      newString.length,
+      "SearchedString",
+      false
+    );
+    // update the occurrencesIndices.
+    this.search();
+  }
 
-        let oldString = document.getElementById('search-input').value;
-        let newString = document.getElementById('replace-input').value;
+  replaceAll() {
+    if (!Searcher.SearchedString) return;
+    let oldStringLen = document.getElementById("search-input").value.length;
+    let newString = document.getElementById("replace-input").value;
 
-        quill.deleteText(indices[Searcher.currentIndex], oldString.length);
-        quill.insertText(indices[Searcher.currentIndex], newString);
-        quill.formatText(indices[Searcher.currentIndex], newString.length, 'SearchedString', false);
+    // if no occurrences, then search first.
+    if (!Searcher.occurrencesIndices) this.search();
+    if (!Searcher.occurrencesIndices) return;
+
+    if (Searcher.occurrencesIndices) {
+      while (Searcher.occurrencesIndices) {
+        quill.deleteText(Searcher.occurrencesIndices[0], oldStringLen);
+        quill.insertText(Searcher.occurrencesIndices[0], newString);
+
         // update the occurrencesIndices.
         this.search();
+      }
     }
+    Searcher.removeStyle();
+  }
 
-    replaceAll() {
-        if (!Searcher.SearchedString) return;
-        let oldStringLen = document.getElementById('search-input').value.length;
-        let newString = document.getElementById('replace-input').value;
-
-        // if no occurrences, then search first.
-        if (!Searcher.occurrencesIndices) this.search();
-        if (!Searcher.occurrencesIndices) return;
-
-
-        if (Searcher.occurrencesIndices) {
-            while (Searcher.occurrencesIndices) {
-                quill.deleteText(Searcher.occurrencesIndices[0], oldStringLen);
-                quill.insertText(Searcher.occurrencesIndices[0], newString);
-
-                // update the occurrencesIndices.
-                this.search();
-            }
-        }
-        Searcher.removeStyle();
+  static removeStyle() {
+    if (Searcher.occurrencesIndices) {
+      Searcher.occurrencesIndices.forEach(index =>
+        quill.formatText(
+          index,
+          Searcher.SearchedStringLength,
+          "SearchedString",
+          false
+        )
+      );
     }
-
-    static removeStyle() {
-        if (Searcher.occurrencesIndices) {
-            Searcher.occurrencesIndices.forEach(index =>
-                quill.formatText(index, Searcher.SearchedStringLength, 'SearchedString', false
-                )
-            );
-        }
-    }
+  }
 }
+
+occurrencesIndices = [];
+currentIndex = 0;
+SearchedStringLength = 0;
+SearchedString = "";
+
+// function for utility
+String.prototype.getIndicesOf = function(searchStr) {
+  let searchStrLen = searchStr.length;
+  let startIndex = 0,
+    index,
+    indices = [];
+  while ((index = this.toLowerCase().indexOf(searchStr.toLowerCase(), startIndex)) > -1) {
+    indices.push(index);
+    startIndex = index + searchStrLen;
+  }
+  return indices;
+};
+
 export default Searcher;
